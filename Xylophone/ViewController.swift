0b1ea3c9cf93f2786dcd141617ba78edd76ba421
ravi <ravinavarro26@gmail.com>
2021-01-23 23:26:00 -0300

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+    playSound()
+        
+    }
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {
+            return
+        }
 
+        do {
+            /// this codes for making this app ready to takeover the device audio
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /// change fileTypeHint according to the type of your audio file (you can omit this)
+
+            /// for iOS 11 onward, use :
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /// else :
+            /// player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3)
+
+            // no need for prepareToPlay because prepareToPlay is happen automatically when calling play()
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print("error: \(error.localizedDescription)")
+        }
+    }
     
-
+    
+    
 }
-
